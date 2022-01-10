b529abc095d926acc580b3705b3c2aee23fa6cf9
hadiamann2006 <97448529+hadiamann2006@users.noreply.github.com>
2022-01-10 14:21:42 +0100

    Update Notendurchschnittsrechner2 copy.py

--- Notendurchschnittsrechner2 copy.py
+++ Notendurchschnittsrechner2 copy.py
@@ -1,3 +1,23 @@
-# Mein Code
+noten=[]
+
+while True:
+    note = float(input("Note hinzufügen: "))
+    noten.append(note)
+
+    # durchschnitt berechnen
+    summe = 0
+    n = len(noten)
+    for note in noten:
+        summe = summe + note
+    durchschnitt = summe / n
+
+    print("------------------------")
+    print("Noten: " + str(noten))
+    print("Durchschnitt: " + str(durchschnitt))
+    print("------------------------")
+    
+
+# Notendurchschnittsberechner
+## Einleitung: Mit diesem Program kann man den persönlichen Notendurchschnitt berechnen. Dabei müssen Sie ihre gesamten Noten angeben. Sofort wird Ihnen ihr momentaner Notendurchschnitt angegeben.
     
 
