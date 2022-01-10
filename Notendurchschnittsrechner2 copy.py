0aa9f6583be32c59532cdbaa65ee3d3c83d1409f
hadiamann2006 <97448529+hadiamann2006@users.noreply.github.com>
2022-01-10 13:56:58 +0100

    Update Notendurchschnittsrechner2 copy.py

--- Notendurchschnittsrechner2 copy.py
+++ Notendurchschnittsrechner2 copy.py
@@ -1,10 +1,3 @@
-summe=0
-n=0
-while True:
-    note=float(input("was ist die nächste Note? "))
-    n=n+1
-    summe=summe+note
-    durchschnittlich=summe/n
-    print("Durchschnitt:")
+# Mein Code
     
 
